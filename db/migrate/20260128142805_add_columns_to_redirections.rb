9553d95ac1d6966e720f9c54d8728540653b99be
Tomasz Strzeszewski <tomasz.strzeszewski@yes.pl>
2026-02-10 09:40:21 +0100

    typo

--- db/migrate/20260128142805_add_columns_to_redirections.rb
+++ db/migrate/20260128142805_add_columns_to_redirections.rb
@@ -5,6 +5,6 @@ class AddColumnsToRedirections < ActiveRecord::Migration[7.2]
     add_column :redirections, :deleted_by, :string, default: 'Unknown'
     add_column :redirections, :created_by, :string, default: 'Unknown'
 
-    SpreeRedirections::Redirection.where(created_by: nil).update_all(created_by: 'unknown') # rubocop:disable Rails/SkipsModelValidations
+    SpreeRedirections::Redirection.where(created_by: nil).update_all(created_by: 'Unknown') # rubocop:disable Rails/SkipsModelValidations
   end
 end
